b01733184d6fb4e620ee27daa8c26d20057f9d89
= <richardoehbet221@gmail.com>
2021-12-01 22:28:36 +0700

    Update axios base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://catwiki-backend-io.herokuapp.com/api";
 const instance = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:8080/api",
+  baseURL: API_URL,
 });
 
 export default instance;
